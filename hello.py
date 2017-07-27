d15c43b0b027132323d7253994d3ea31cd2d485c
SheldonZheng <zhenghangtxdyr@gmail.com>
2017-07-27 10:33:42 +0800

    Add Summary Image for MNIST

--- hello.py
+++ hello.py
@@ -1,36 +1,71 @@
 from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
 
-mnist = input_data.read_data_sets("MNIST_data/",one_hot=True)
 
-print(mnist.train.images.shape,mnist.train.labels.shape)
-print(mnist.test.images.shape,mnist.test.labels.shape)
-print(mnist.validation.images.shape,mnist.validation.labels.shape)
+# print(mnist.train.images.shape,mnist.train.labels.shape)
+# print(mnist.test.images.shape,mnist.test.labels.shape)
+# print(mnist.validation.images.shape,mnist.validation.labels.shape)
 
-sess = tf.InteractiveSession()
-x = tf.placeholder(tf.float32,[None,784])
-W = tf.Variable(tf.zeros([784,10]))
-b = tf.Variable(tf.zeros([10]))
+flags = tf.app.flags
+FLAGS = flags.FLAGS
 
-# true y
-y_ = tf.placeholder(tf.float32,[None,10])
+flags.DEFINE_string('summaries_dir','/Users/zhenghang/Project/tensorflow-l/mnist_log','Summaries directory')
+
+def train() :
+    mnist = input_data.read_data_sets("MNIST_data/",one_hot=True)
+
+    sess = tf.InteractiveSession()
+
+    with tf.name_scope('input') :
+        x = tf.placeholder(tf.float32,[None,784], name = 'x_input')
+        image_shaped_input = tf.reshape(x,[-1,28,28,1])
+        tf.summary.image('input',image_shaped_input,10)
+        # true y
+        y_ = tf.placeholder(tf.float32,[None,10],name = 'y_input')
+
+
+    W = tf.Variable(tf.zeros([784,10]))
+    b = tf.Variable(tf.zeros([10]))
 
 # predict y  hypothesis function
-y = tf.nn.softmax(tf.matmul(x,W) + b)
+    y = tf.nn.softmax(tf.matmul(x,W) + b)
 
+    with tf.name_scope('cross_entropy'):
 #cost function
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y),reduction_indices=[1]))
+        cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y),reduction_indices=[1]))
 
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
-tf.global_variables_initializer().run()
+    with tf.name_scope('accuracy'):
+        with tf.name_scope('correct_prediction'):
+            correct_prediction = tf.equal(tf.argmax(y,1) , tf.argmax(y_,1))
+        with tf.name_scope('accuracy'):
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        tf.summary.scalar('accuracy',accuracy)
 
-for i in range(1000) :
-    batch_xs,batch_ys = mnist.train.next_batch(100)
-    train_step.run({x : batch_xs, y_ : batch_ys})
+    merged = tf.summary.merge_all()
+    train_writer = tf.summary.FileWriter(FLAGS.summaries_dir + '/train',sess.graph)
+    tf.global_variables_initializer().run()
+    tf.initialize_all_variables().run()
 
-correct_prediction = tf.equal(tf.argmax(y,1) , tf.argmax(y_,1))
+    for i in range(1000) :
+        batch_xs,batch_ys = mnist.train.next_batch(100)
+        summary,_ = sess.run([merged,train_step],feed_dict={x : batch_xs, y_ : batch_ys})
+        train_writer.add_summary(summary,i)
+        #train_step.run({x : batch_xs, y_ : batch_ys})
+        #print(accuracy.eval({x : mnist.test.images, y_ : mnist.test.labels}))
 
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+if __name__ == '__main__':
+    train()
 
-print(accuracy.eval({x : mnist.test.images, y_ : mnist.test.labels}))
+# def variable_summaries(var,name):
+#     with tf.name_scope('summaries'):
+#         mean = tf.reduce_mean(var)
+#         tf.scalar_summary('mean/' + name,mean)
+#         with tf.name_scope('stddev'):
+#             stddev = tf.sqrt(tf.reduce_sum(tf.square(var - mean))
+#
+#         tf.scalar_summary('stddev/' + name,stddev)
+#         tf.scalar_summary('max/' + name,tf.reduce_max(var))
+#         tf.scalar_summary('min/' + name,tf.reduce_min(var))
+#         tf.histogram_summart(name,var)

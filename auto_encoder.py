74e327285d25783216f9fcbcd0b10b3f90198dbf
SheldonZheng <zhenghangtxdyr@gmail.com>
2017-08-03 11:04:30 +0800

    Solve AutoEncoder

---
+++ auto_encoder.py
@@ -0,0 +1,9 @@
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
+import numpy as np
+import sklearn.preprocessing as prep
+
+def xavier_init(fan_in,fan_out,constant = 1):
+    low = -constant * np.sqrt(6.0 / (fan_in + fan_out))
+    high = constant * np.sqrt(6.0 / (fan_in + fan_out))
+    return tf.random_uniform((fan_in,fan_out),minval = low,maxval = high,dtype=tf.float32)
